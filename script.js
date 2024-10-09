0b2299f7178a21649069d27857b8e2c140ba135f
Shubham Maurya <157043197+Skmaurya05550@users.noreply.github.com>
2024-10-09 19:36:10 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+var p = true;
+
+function toggle() {
+    if (p) {
+        document.documentElement.style.cssText = '--s: #ffdd80; --o: 0px; --c: #aacaff; --f: #efefef; --n: 60px; --t: 0px; --d: #2222252a;';
+    } else {
+        document.documentElement.style.cssText = '--s: #424248; --o: -15px; --c: #222222; --f: #131313; --n: 0px; --t: 10px; --d: #eaeaff2a;';
+    }
+    p = !p;
+}
